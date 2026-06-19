7df1c65b0a77286f8a9cde427ce09fe2f7365cd0
kit sitou <ctwohofa@gmail.com>
2026-06-18 17:31:32 -0700

    Initial Commit

---
+++ MyCamera/MyCameraApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyCameraApp.swift
+//  MyCamera
+//
+//  Created by Kit Sitou on 6/18/26.
+//
+
+import SwiftUI
+
+@main
+struct MyCameraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
